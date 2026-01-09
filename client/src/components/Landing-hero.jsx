efbd48d2d899b61fc03b0da922fd6e9e15c91279
dayone.codes@gmail.com <dayone.codes@gmail.com>
2026-01-09 11:08:00 +0000

    update landing page hero

--- client/src/components/Landing-hero.jsx
+++ client/src/components/Landing-hero.jsx
@@ -3,7 +3,7 @@ import bgLandingPage from "../assets/club-ii.jpg";
 function LandingHero ({onScrollToEvents, onScrollToServices}) {
   return (
     <>
-      <div className="relative pt-20 min-h-[90vh] flex items-center justify-center overflow-hidden">
+      <div className="relative pt-20 min-h-[90vh] flex items-center justify-center overflow-hidden" data-header-theme="light">
 
       <div className="absolute inset-0 z-0 bg-cover bg-center bg-no-repeat bg-fixed" data-alt="Crowd at a concert with purple lighting and confetti" style={{
           backgroundImage: `url("${bgLandingPage}")
@@ -11,13 +11,13 @@ function LandingHero ({onScrollToEvents, onScrollToServices}) {
           }}>
       </div>
       <div className="relative z-10 container mx-auto px-4 md:px-10 flex flex-col items-center text-center gap-8 max-w-4xl">
-          <span className="text-white text-sm font-bold tracking-widest uppercase animate-pulse">-D WAVE ENTERTAINMENT-</span>
-          <h1 className="text-4xl md:text-7xl lg:text-8xl font-black text-white leading-[0.9] tracking-tighter drop-shadow-2xl">
-          UNFORGETTABLE<br/>
-            <span className="text-transparent bg-clip-text bg-white ">NIGHTS.</span>
-          </h1>
-          <p className="text-gray-300 text-lg md:text-xl font-medium max-w-2xl leading-relaxed">
-          We curate world-class events and manage top-tier talent. Join the movement where music meets lifestyle.
+          <span className="text-white text-sm font-bold tracking-widest uppercase animate-pulse">Coventry &amp; West Midlands</span>
+ <h1 class="text-4xl md:text-7xl lg:text-8xl font-black text-white leading-[1] tracking-tighter text-left md:text-center mb-8">
+                    D-WAVE ENTERTAINMENT: <br/><span class="text-gradient">CULTURALLY RELEVANT EXPERIENCES</span>
+</h1>
+
+          <p className="text-white text-lg md:text-xl font-medium max-w-2xl leading-relaxed">
+          Structured nightlife, sports, top-tier talent management and community engagement tailored for the modern West Midlands landscape.
           </p>
           <div className="flex flex-col sm:flex-row gap-4 w-full sm:w-auto mt-6">
               <button className="bg-white hover:bg-background-light/80 hover: border hover:border-white  text-primary text-base font-bold h-14 px-8 rounded-lg shadow-[0_0_20px_rgba(40,24,40,0.4)] hover:shadow-[0_0_30px_[0_0_30px_rgba(40,24,40,0.6)] transition-all flex items-center justify-center gap-2" onClick={() => onScrollToEvents()
