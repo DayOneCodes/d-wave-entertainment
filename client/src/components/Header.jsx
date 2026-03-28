845db7d5275381e298ec2edad952f42fe1867747
dayone.codes@gmail.com <dayone.codes@gmail.com>
2026-03-28 12:49:54 +0000

    [chore]: Update Header UI to include brand logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,55 +17,58 @@ useEffect(() => {
     <>
       <nav className={`fixed top-0 left-0 right-0 z-50 backdrop-blur-md bg-primary  border-b transition-all transition-200 border-gray-200`}>
           <div className="layout-container flex h-[70px] grow flex-col">
-          <div className="px-4 md:px-10 lg:px-40 flex justify-center">
-          <div className="layout-content-container flex flex-col md:flex-row gap-4 w-full max-w-[1200px] items-center justify-between py-4">
-          <div className="flex items-center gap-4 text-slate-900" onClick={
-           () => {navigate("/")}
-          }>
-              <div className="size-8 text-primary hidden md:block">
-                  <img src="https://res.cloudinary.com/dslzm3lo6/image/upload/v1771483783/logo_rbnlbh.jpg" alt="D Wave Entertainment Logo" className="w-full h-full object-contain"/>
-              </div>
-          </div>
-          <div className="hidden md:flex flex-1 justify-end gap-8 items-center">
-              <div className="flex items-center gap-9">
-                  <a className="text-sm text-white font-medium transition-colors cursor-pointer" onClick={() => onScrollToEvents()}>Upcoming Events</a>
-                  <NavLink className="text-sm text-white font-medium transition-colors" to="/about-us">About Us</NavLink>
-                  <a className="text-sm text-white font-medium transition-colors cursor-pointer" onClick={() => onScrollToServices()}>Services</a>
-                  <NavLink className="text-sm text-white font-medium transition-colors" to="/contact">Contact</NavLink>
-              </div>
-              { !isAuthenticated && (
-              <button className="bg-white hover:scale-105 transition-all text-primary text-sm font-bold h-10 px-6 rounded-lg shadow-[0_0_20px_rgba(40,24,40,0.4)] hover:shadow-[0_0_30px_rgba(40,24,40,0.6)]" onClick={ () =>
-                navigate("/auth")
-              }>
-                                        Sign In
-              </button> )}
-              {
-                isAuthenticated && (
-                  <> 
-                  <span className="text-white cursor-pointer" onClick={() => {
-                    if (user.role === "admin") {
-                      navigate("/admin");
-                    } else if (user.role === "user") {
-                      navigate("/dashboard");
-                    }
-                  }}>
-                    {user.role === "user" ? "Dashboard" : "Admin"}</span>
-                  <button className="bg-white hover:scale-105 transition-all text-primary text-sm font-bold h-10 px-6 rounded-lg shadow-[0_0_20px_rgba(40,24,40,0.4)] hover:shadow-[0_0_30px_rgba(40,24,40,0.6)]" onClick={
-                    () => useLogout()
-                  }>Logout</button>
-                  </>
-                )
-              }
-          </div>
-          {/* <!-- Mobile Menu Icon --> */}
-          <div className={` text-white opacity-85 bg-primary flex justify-center items-center rounded-sm p-1 md:hidden transition-all transition-200 text-bold`}>
-          <span className="material-symbols-outlined" onClick={ () => {
-            setMobileNavOpen(!mobileNavOpen)
-          } 
-          }>
-            menu
-          </span>
-          </div>
+          <div className="px-6 md:px-10 lg:px-40 flex justify-center">
+          <div className="layout-content-container flex flex-row gap-4 w-full max-w-[1200px] items-center justify-between py-4">
+
+            <div className="flex items-center gap-4 text-slate-900" onClick={
+            () => {navigate("/")}
+            }>
+                <div className="size-8 text-primary">
+                    <img src="https://res.cloudinary.com/dslzm3lo6/image/upload/v1771483783/logo_rbnlbh.jpg" alt="D Wave Entertainment Logo" className="w-full h-full object-contain"/>
+                </div>
+            </div>
+
+            <div className="hidden md:flex flex-1 justify-end gap-8 items-center">
+                <div className="flex items-center gap-9">
+                    <a className="text-sm text-white font-medium transition-colors cursor-pointer" onClick={() => onScrollToEvents()}>Upcoming Events</a>
+                    <NavLink className="text-sm text-white font-medium transition-colors" to="/about-us">About Us</NavLink>
+                    <a className="text-sm text-white font-medium transition-colors cursor-pointer" onClick={() => onScrollToServices()}>Services</a>
+                    <NavLink className="text-sm text-white font-medium transition-colors" to="/contact">Contact</NavLink>
+                </div>
+                { !isAuthenticated && (
+                <button className="bg-white hover:scale-105 transition-all text-primary text-sm font-bold h-10 px-6 rounded-lg shadow-[0_0_20px_rgba(40,24,40,0.4)] hover:shadow-[0_0_30px_rgba(40,24,40,0.6)]" onClick={ () =>
+                  navigate("/auth")
+                }>
+                                          Sign In
+                </button> )}
+                {
+                  isAuthenticated && (
+                    <> 
+                    <span className="text-white cursor-pointer" onClick={() => {
+                      if (user.role === "admin") {
+                        navigate("/admin");
+                      } else if (user.role === "user") {
+                        navigate("/dashboard");
+                      }
+                    }}>
+                      {user.role === "user" ? "Dashboard" : "Admin"}</span>
+                    <button className="bg-white hover:scale-105 transition-all text-primary text-sm font-bold h-10 px-6 rounded-lg shadow-[0_0_20px_rgba(40,24,40,0.4)] hover:shadow-[0_0_30px_rgba(40,24,40,0.6)]" onClick={
+                      () => useLogout()
+                    }>Logout</button>
+                    </>
+                  )
+                }
+            </div>
+
+            {/* <!-- Mobile Menu Icon --> */}
+            <div className={` text-white opacity-85 bg-primary flex justify-center items-center rounded-sm p-1 md:hidden transition-all transition-200 text-bold`}>
+            <span className="material-symbols-outlined" onClick={ () => {
+              setMobileNavOpen(!mobileNavOpen)
+            } 
+            }>
+              menu
+            </span>
+            </div>
           </div>
           </div>
           </div>
